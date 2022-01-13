070c6ceb864465c6ce4e9c2dfa924cfbde805ad2
Егор Дюков <90068563+DukovEgor@users.noreply.github.com>
2022-01-13 11:39:01 +0300

    2. Обработка ошибок абстрактного класса

--- src/view/absract-view.js
+++ src/view/absract-view.js
@@ -2,6 +2,12 @@ import { createElement } from '../render.js';
 
 export default class AbstractView {
   #element = null;
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Can\'t instantiate AbstractView, only concrete one.');
+    }
+  }
+
 
   get element() {
     if (!this.#element) {
@@ -9,6 +15,10 @@ export default class AbstractView {
     }
 
     return this.#element;
+  }
+
+  get template() {
+    throw new Error('Abstract method not implemented: get template');
   }
 
   removeElement() {
