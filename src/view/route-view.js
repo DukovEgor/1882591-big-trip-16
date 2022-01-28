e321f1eb0935df5649e12baa5ae8c8dc35fe2c67
Егор Дюков <90068563+DukovEgor@users.noreply.github.com>
2022-01-28 07:44:11 +0300

    10. Подсчет общей продолжительности путешествия

--- src/view/route-view.js
+++ src/view/route-view.js
@@ -1,4 +1,5 @@
 
+import dayjs from 'dayjs';
 import { howManyCities } from '../utils/utils.js';
 import AbstractView from './absract-view.js';
 
@@ -10,13 +11,19 @@ const calcTotal = (arr) => {
   );
 };
 
+const showTripDuration = (arr) => {
+  const firstDate = dayjs(arr[0].dateFrom).format('MMM D');
+  const lastDate = arr.length > 1 ? dayjs(arr[arr.length - 1].dateTo).format('MMM D'): null;
+  return `${firstDate}${lastDate !== null ? `&nbsp;&mdash;&nbsp;${dayjs(lastDate).isSame(dayjs(firstDate), 'M') ? dayjs(lastDate).format('D') : lastDate}` : ''}`;
+};
+
 
 const createRoute = (arr) => (
   `<section class="trip-main__trip-info  trip-info">
 <div class="trip-info__main">
   <h1 class="trip-info__title">${howManyCities(arr)}</h1>
 
-  <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+  <p class="trip-info__dates">${showTripDuration(arr)}</p>
 </div>
 
 <p class="trip-info__cost">
